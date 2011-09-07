63312fec4804002eaed51da57cec190211739325
Omar Benhamid <none@none>
2011-09-07 16:04:22 +0200

    Working v0

--- src/main/java/com/ericsson/sn/mobilefaces/MobileRenderKitFactory.java
+++ src/main/java/com/ericsson/sn/mobilefaces/MobileRenderKitFactory.java
@@ -25,90 +25,113 @@ package com.ericsson.sn.mobilefaces;
 import java.util.HashMap;
 import java.util.Iterator;
 
-import javax.faces.render.RenderKit;
 import javax.faces.context.FacesContext;
+import javax.faces.render.RenderKit;
 import javax.faces.render.RenderKitFactory;
 
-import com.ericsson.sn.mobilefaces.util.Log;
 import com.ericsson.sn.mobilefaces.device.DeviceDataFactory;
 import com.ericsson.sn.mobilefaces.renderkit.html.HTMLRenderKit;
+import com.ericsson.sn.mobilefaces.util.Log;
 
 /**
  * <p>
- *      MobileRenderKitFactory is an implementation of RenderKitFactory
- *      for generating RenderKit for mobile devices based on
- *      {@link com.ericsson.sn.mobilefaces.device.DeviceData} feature.
+ * MobileRenderKitFactory is an implementation of RenderKitFactory for
+ * generating RenderKit for mobile devices based on
+ * {@link com.ericsson.sn.mobilefaces.device.DeviceData} feature.
  * </p>
- *
+ * 
  * <p>
- *      MobileRenderKitFactory contains HTML_BASIC/XHTML-MP/WML renderkits
- *      for mobile device. New renderkits can be defined in faces-config as
- *      standard JSF custom renderkit.
+ * MobileRenderKitFactory contains HTML_BASIC/XHTML-MP/WML renderkits for mobile
+ * device. New renderkits can be defined in faces-config as standard JSF custom
+ * renderkit.
  * </p>
- *
- * <p>Company:   Ericsson AB</p>
- *
- *
- *
+ * 
+ * <p>
+ * Company: Ericsson AB
+ * </p>
+ * 
+ * 
+ * 
  * @author Daning Yang
  * @version 1.0
  */
 public class MobileRenderKitFactory extends RenderKitFactory {
 
-    protected String renderKitId = null;
-    protected HashMap<String, RenderKit> renderKits = null;
+	protected String renderKitId = null;
+	protected HashMap<String, RenderKit> renderKits = null;
 
-    /**
-     * <p> Initialize renderkit pool and default HTML renderkit. </p>
-     */
-    public MobileRenderKitFactory() {
-        super();
-        renderKits = new HashMap<String, RenderKit>();
-        addRenderKit(HTML_BASIC_RENDER_KIT, new HTMLRenderKit());
-    }
+	/**
+	 * <p>
+	 * Initialize renderkit pool and default HTML renderkit.
+	 * </p>
+	 */
+	public MobileRenderKitFactory() {
+		super();
+		renderKits = new HashMap<String, RenderKit>();
+		addRenderKit(HTML_BASIC_RENDER_KIT, new HTMLRenderKit());
+	}
 
-    /**
-     * <p> Add new renderkit to pool </p>
-     * @param renderKitId new renderkit ID
-     * @param renderKit new renderkit
-     */
-    @Override
-    public void addRenderKit(String renderKitId, RenderKit renderKit) {
-        synchronized (renderKits) {
-            Log.debug("Add renderkit: " + renderKitId, MobileRenderKitFactory.class);
-            renderKits.put(renderKitId, renderKit);
-        }
-    }
+	/**
+	 * <p>
+	 * Add new renderkit to pool
+	 * </p>
+	 * 
+	 * @param renderKitId
+	 *            new renderkit ID
+	 * @param renderKit
+	 *            new renderkit
+	 */
+	@Override
+	public void addRenderKit(String renderKitId, RenderKit renderKit) {
+		synchronized (renderKits) {
+			Log.debug("Add renderkit: " + renderKitId,
+					MobileRenderKitFactory.class);
+			renderKits.put(renderKitId, renderKit);
+		}
+	}
 
-    /**
-     * <p> Get renderkit from pool based on current device support markup language. </p>
-     * @param context FacesContext to get device feature
-     * @param renderKitId default renderKit Id
-     * @return renderkit
-     */
-    @Override
-    public RenderKit getRenderKit(FacesContext context, String renderKitId) {
-        if(context != null) {
-            renderKitId = DeviceDataFactory.getCurrentDevice().getMarkup();
-        }
-        RenderKit renderKit = null;
-        synchronized (renderKits) {
-            if (renderKits.containsKey(renderKitId)) {
-                renderKit = renderKits.get(renderKitId);
-            }
-        }
-        if (context != null)
-            Log.doAssert( renderKit, "Cannot find renderkit " + renderKitId, MobileRenderKitFactory.class);
+	/**
+	 * <p>
+	 * Get renderkit from pool based on current device support markup language.
+	 * </p>
+	 * 
+	 * @param context
+	 *            FacesContext to get device feature
+	 * @param renderKitId
+	 *            default renderKit Id
+	 * @return renderkit
+	 */
+	@Override
+	public RenderKit getRenderKit(FacesContext context, String renderKitId) {
+		if (context != null) {
+			try {
+				renderKitId = DeviceDataFactory.getCurrentDevice().getMarkup();
+			} catch (NullPointerException npe) {
+				renderKitId = HTML_BASIC_RENDER_KIT;
+			}
+		}
+		RenderKit renderKit = null;
+		synchronized (renderKits) {
+			if (renderKits.containsKey(renderKitId)) {
+				renderKit = renderKits.get(renderKitId);
+			}
+		}
+		if (context != null)
+			Log.doAssert(renderKit, "Cannot find renderkit " + renderKitId,
+					MobileRenderKitFactory.class);
 
-        return renderKit;
-    }
+		return renderKit;
+	}
 
-    /**
-     * <p> Get iterator for all renderkits in the pool. </p>
-     * @return iterator of all renderkits
-     */
-    @Override
-    public Iterator<String> getRenderKitIds() {
-        return renderKits.keySet().iterator();
-    }
+	/**
+	 * <p>
+	 * Get iterator for all renderkits in the pool.
+	 * </p>
+	 * 
+	 * @return iterator of all renderkits
+	 */
+	@Override
+	public Iterator<String> getRenderKitIds() {
+		return renderKits.keySet().iterator();
+	}
 }
--- src/main/java/com/ericsson/sn/mobilefaces/renderkit/share/BasicRenderKit.java
+++ src/main/java/com/ericsson/sn/mobilefaces/renderkit/share/BasicRenderKit.java
@@ -22,132 +22,178 @@
  */
 package com.ericsson.sn.mobilefaces.renderkit.share;
 
-import java.util.HashMap;
-import java.io.Writer;
 import java.io.OutputStream;
+import java.io.Writer;
+import java.util.HashMap;
 
+import javax.faces.context.ResponseStream;
+import javax.faces.context.ResponseWriter;
+import javax.faces.render.ClientBehaviorRenderer;
 import javax.faces.render.RenderKit;
 import javax.faces.render.Renderer;
 import javax.faces.render.ResponseStateManager;
-import javax.faces.context.ResponseStream;
-import javax.faces.context.ResponseWriter;
 
-import com.ericsson.sn.mobilefaces.MobileResponseWriter;
 import com.ericsson.sn.mobilefaces.MobileResponseStateManager;
+import com.ericsson.sn.mobilefaces.MobileResponseWriter;
 
 /**
  * <p>
- *      Basic renderkit implements basic methods for
- *      different markup langugae renderkit.
+ * Basic renderkit implements basic methods for different markup langugae
+ * renderkit.
  * </p>
- *
- * <p>Company:   Ericsson AB</p>
- *
- *
+ * 
+ * <p>
+ * Company: Ericsson AB
+ * </p>
+ * 
+ * 
  * @author Daning Yang
  * @version 1.0
  */
 public abstract class BasicRenderKit extends RenderKit {
 
-    /**
-     * <p>renderer kit.</p>
-     */
-    protected HashMap<String, Renderer> renderKit;
-    /**
-     * <p>Response State Manager</p>
-     */
-    protected ResponseStateManager responseStateManager = null;
+	/**
+	 * <p>
+	 * renderer kit.
+	 * </p>
+	 */
+	protected HashMap<String, Renderer> renderKit;
+	/**
+	 * <p>
+	 * Response State Manager
+	 * </p>
+	 */
+	protected ResponseStateManager responseStateManager = null;
 
-    /**
-     * <p>Create a instance of BasicRenderKit.</p>
-     */
-    public BasicRenderKit() {
-        super();
-        responseStateManager = new MobileResponseStateManager();
-        renderKit = new HashMap<String, Renderer>();
-    }
+	/**
+	 * <p>
+	 * Create a instance of BasicRenderKit.
+	 * </p>
+	 */
+	public BasicRenderKit() {
+		super();
+		responseStateManager = new MobileResponseStateManager();
+		renderKit = new HashMap<String, Renderer>();
+	}
 
-    /**
-     * <p>Return the content type</p>
-     * @return content type
-     */
-    protected abstract String getContentType();
+	/**
+	 * <p>
+	 * Return the content type
+	 * </p>
+	 * 
+	 * @return content type
+	 */
+	protected abstract String getContentType();
 
-    /**
-     * <p>Return the encoding</p>
-     * @return encoding
-     */
-    protected abstract String getEncoding();
+	/**
+	 * <p>
+	 * Return the encoding
+	 * </p>
+	 * 
+	 * @return encoding
+	 */
+	protected abstract String getEncoding();
 
-    /**
-     * <p>Add a renderer to the manager</p>
-     * @param family renderer family
-     * @param rendererType renderer type
-     * @param renderer renderer
-     */
-    @Override
-    public void addRenderer(String family, String rendererType, Renderer renderer) {
-        synchronized (renderKit) {
-            renderKit.put(family + "." + rendererType, renderer);
-        }
-    }
+	/**
+	 * <p>
+	 * Add a renderer to the manager
+	 * </p>
+	 * 
+	 * @param family
+	 *            renderer family
+	 * @param rendererType
+	 *            renderer type
+	 * @param renderer
+	 *            renderer
+	 */
+	@Override
+	public void addRenderer(String family, String rendererType,
+			Renderer renderer) {
+		synchronized (renderKit) {
+			renderKit.put(family + "." + rendererType, renderer);
+		}
+	}
 
-    /**
-     * <p>Get a renderer from the manager</p>
-     * @param family renderer family
-     * @param rendererType renderer type
-     * @return renderer
-     */
-    @Override
-    public Renderer getRenderer(String family, String rendererType) {
-        Renderer renderer = null;
-        StringBuffer sb = new StringBuffer(family);
-        sb.append(".");
-        sb.append(rendererType);
-        synchronized (renderKit) {
-            renderer = renderKit.get(sb.toString());
-        }
-        return renderer;
-    }
+	/**
+	 * <p>
+	 * Get a renderer from the manager
+	 * </p>
+	 * 
+	 * @param family
+	 *            renderer family
+	 * @param rendererType
+	 *            renderer type
+	 * @return renderer
+	 */
+	@Override
+	public Renderer getRenderer(String family, String rendererType) {
+		Renderer renderer = null;
+		StringBuffer sb = new StringBuffer(family);
+		sb.append(".");
+		sb.append(rendererType);
+		synchronized (renderKit) {
+			renderer = renderKit.get(sb.toString());
+		}
+		return renderer;
+	}
 
-    /**
-     * <p>Get a Response State Manager</p>
-     * @return a Response State Manager
-     */
-    @Override
-    public ResponseStateManager getResponseStateManager() {
-        return responseStateManager;
-    }
+	/**
+	 * <p>
+	 * Get a Response State Manager
+	 * </p>
+	 * 
+	 * @return a Response State Manager
+	 */
+	@Override
+	public ResponseStateManager getResponseStateManager() {
+		return responseStateManager;
+	}
 
-    /**
-     * <p>Get a response writer instance.</p>
-     * @param writer original writer
-     * @param contentTypeList content type list
-     * @param characterEncoding character encoding
-     * @return response writer instance
-     */
-    @Override
-    public ResponseWriter createResponseWriter(Writer writer, String contentTypeList,
-            String characterEncoding) {
-        if (writer == null) {
-            return null;
-        }
-        // Set content type and encoding
-        String contentType = getContentType();
-        if (characterEncoding == null) {
-            characterEncoding = getEncoding();
-        }
-        // Create a instance
-        return new MobileResponseWriter(writer, contentType, characterEncoding);
-    }
+	/**
+	 * <p>
+	 * Get a response writer instance.
+	 * </p>
+	 * 
+	 * @param writer
+	 *            original writer
+	 * @param contentTypeList
+	 *            content type list
+	 * @param characterEncoding
+	 *            character encoding
+	 * @return response writer instance
+	 */
+	@Override
+	public ResponseWriter createResponseWriter(Writer writer,
+			String contentTypeList, String characterEncoding) {
+		if (writer == null) {
+			return null;
+		}
+		// Set content type and encoding
+		String contentType = getContentType();
+		if (characterEncoding == null) {
+			characterEncoding = getEncoding();
+		}
+		// Create a instance
+		return new MobileResponseWriter(writer, contentType, characterEncoding);
+	}
 
-    /**
-     * <p>Get a Response Stream instance.</p>
-     * @param out original output stream
-     * @return a Response Stream instance
-     */
-    @Override
-    public ResponseStream createResponseStream(OutputStream out) {
-        return null;
-    }
+	/**
+	 * <p>
+	 * Get a Response Stream instance.
+	 * </p>
+	 * 
+	 * @param out
+	 *            original output stream
+	 * @return a Response Stream instance
+	 */
+	@Override
+	public ResponseStream createResponseStream(OutputStream out) {
+		return null;
+	}
+
+	@Override
+	public void addClientBehaviorRenderer(String type,
+			ClientBehaviorRenderer renderer) {
+		// Dumb do nothing impl
+	}
 }
